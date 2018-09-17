4d7bbbe41910584178f41cbb64a7741060072087
alexorear <alex.orear@gmail.com>
2018-09-17 12:34:59 -0500

    Set up dummy todo items and addItem/viewItem methods.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ToDoItem } from '../../interfaces/todo-item';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	public items: ToDoItem[];
 
-  }
+	constructor(public navCtrl: NavController) {
+
+	}
+
+	ngOnInit() {
+		this.items = [
+			{title: 'test1', description: 'First test item'},
+			{title: 'test2', description: 'Second test item'},
+			{title: 'test3', description: 'Third test item'},
+			{title: 'test4', description: 'Fourth test item'}
+		]
+	}
+
+	addItem() {}
+
+	viewItem() {}
 
 }
