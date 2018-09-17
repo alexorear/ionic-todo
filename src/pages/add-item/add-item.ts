664bd1699a4b78f0e48dd42c5f5c4b1c89bb7de4
alexorear <alex.orear@gmail.com>
2018-09-17 12:53:07 -0500

    Layout for add item template. Create title and description class members. Create saveItem and close class methods.

--- src/pages/add-item/add-item.ts
+++ src/pages/add-item/add-item.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, ViewController } from 'ionic-angular';
+
+import { ToDoItem } from '../../interfaces/todo-item';
 
 @Component({
-  selector: 'page-add-item',
-  templateUrl: 'add-item.html',
+	selector: 'page-add-item',
+	templateUrl: 'add-item.html',
 })
 export class AddItemPage {
+	title: string;
+	description: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+	constructor(
+		public navCtrl: NavController,
+		public view: ViewController
+	) {
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddItemPage');
-  }
+	saveItem() {
+		let newItem: ToDoItem = {
+			title: this.title,
+			description: this.description
+		}
+	}
+
+	close() {
+		this.view.dismiss;
+	}
 
 }
